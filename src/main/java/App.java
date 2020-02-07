2a7101504751ddf99e9211cbd2eba2cae822b7f7
hussein18149 <husseinkatana17@gmail.com>
2020-02-07 13:26:12 +0300

    create a hbs template for the post  routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,8 @@
 import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jetty.client.api.Request;
+
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -6,6 +10,11 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocrats this :)
         staticFileLocation("/public");
+
+        get("/", (Request, response) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
