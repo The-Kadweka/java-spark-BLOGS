e40743d70716a0d3c39d6a9cf5d6c1001826562e
hussein18149 <husseinkatana17@gmail.com>
2020-02-07 13:31:38 +0300

    create a form for the input of the blog

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,11 @@
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.jetty.client.api.Request;
-
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocrats this :)
