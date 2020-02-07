804b223f1224c6076a8d15e9d1e938113a5b6e51
hussein18149 <husseinkatana17@gmail.com>
2020-02-07 13:13:43 +0300

    returning our content string

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,5 +1,13 @@
-public class Post{
-    public Post (String content){
+package models;
+
+public class Post {
+    private String content;
+
+    public Post(String content) {
+        this.content = content;
         
+    }
+    public String getContent(){
+        return content;
     }
 }
