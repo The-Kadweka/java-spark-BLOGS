46a29a352d1a0e7f24d99aa89065827d8a430f87
hussein18149 <husseinkatana17@gmail.com>
2020-02-11 01:55:45 +0300

    defined  methods for deleting and update of the post

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,35 +9,82 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) { //type “psvm + tab” to autocreate this
+    public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
         staticFileLocation("/public");
 
         //get: show new post form
 
-        //post: process new post form
-        post("/posts/new", (req, res) -> { //URL to make new post on POST route
+        get("/posts/new", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "newpost-form.hbs");
+            return new ModelAndView(model, "post-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //post: process new post form
+
+        post("/posts/new", (request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
         //get: show all posts
-        get("/", (req, res) -> {
+
+        get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             ArrayList<Post> posts = Post.getAll();
             model.put("posts", posts);
-
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: delete all posts
+
+        get("/posts/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            Post.clearAllPosts();
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
         //get: show an individual post
 
+        get("/posts/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToFind = Integer.parseInt(request.params("id")); //pull id - must match route segment
+            Post foundPost = Post.findById(idOfPostToFind);    //use it to find post
+            model.put("post", foundPost);   //add it to model for template to display
+            return new ModelAndView(model, "post-detail.hbs");  //individual post page.
+        }, new HandlebarsTemplateEngine());
+
         //get: show a form to update a post
+
+        get("/posts/:id/update", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToEdit = Integer.parseInt(request.params("id"));
+            Post editPost = Post.findById(idOfPostToEdit);
+            model.put("editPost", editPost);
+            return new ModelAndView(model, "post-form.hbs");
+        }, new HandlebarsTemplateEngine());
 
         //post: process a form to update a post
 
+        post("/posts/:id/update", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String newContent = request.queryParams("content");
+            int idOfPostToEdit = Integer.parseInt(request.params("id"));
+            Post editPost = Post.findById(idOfPostToEdit);
+            editPost.update(newContent); //don’t forget me
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
         //get: delete an individual post
 
-        //get: delete all posts
+        get("/posts/:id/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToDelete = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Post deletePost = Post.findById(idOfPostToDelete); //use it to find post
+            deletePost.deletePost();
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
