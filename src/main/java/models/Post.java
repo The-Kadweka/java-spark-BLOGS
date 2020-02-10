abf6d736dd71ecf1a19dcb65613df7224368ba78
hussein18149 <husseinkatana17@gmail.com>
2020-02-11 01:56:51 +0300

    add functions for the  display of the posts pposted

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,21 +1,22 @@
 package models;
 
-import java.util.ArrayList;
-
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Post {
 
     private String content;
     private static ArrayList<Post> instances = new ArrayList<>();
-    private boolean published;
+    private boolean published; //i’m new
     private LocalDateTime createdAt; //see constructor and my method
+    private int id;
 
     public Post (String content){
         this.content = content;
-        this.published = false;
-        this.createdAt = LocalDateTime.now();
         instances.add(this);
+        this.published = false; //also new
+        this.createdAt = LocalDateTime.now();
+        this.id = instances.size();
     }
 
     public String getContent() {
@@ -27,15 +28,31 @@ public class Post {
     }
 
     public static void clearAllPosts(){
-        instances.clear(); //clear as a method is part of the ArrayList class.
+        instances.clear();
     }
 
-    public boolean getPublished(){
+    public boolean getPublished(){ //new too
         return this.published;
     }
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Post findById(int id){
+        return instances.get(id-1); //why minus 1? See if you can figure it out.
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+
+    public void deletePost(){
+        instances.remove(id-1); //same reason
     }
 
 }
